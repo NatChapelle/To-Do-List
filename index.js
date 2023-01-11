5c793361968e3855c15ba098b8bf6e138fe6b5b1
NatChapelle <nathchapelle002@gmail.com>
2023-01-11 16:24:58 +0100

    todo app modif html css

---
+++ index.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("form");
+// console.log(form);
+// ajouter un element
+
+
+
+
+
+form.addEventListener('submit', (e)=> {
+e.preventDefault();
+//on empêche le submit
+//on recupère la valeur de l'input
+console.log(inputfield.value);
+});
+    
