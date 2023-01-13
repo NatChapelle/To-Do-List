8ab714001f306baef70a61506168bd765af357a6
NatChapelle <nathchapelle002@gmail.com>
2023-01-13 18:08:56 +0100

    todo JS evenement au clic et remove

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const form = document.querySelector("form");
 // console.log(form);
+
 // ajouter un element
-
-
-
-
-
 form.addEventListener('submit', (e)=> {
+    //on empêche le submit auto
 e.preventDefault();
-//on empêche le submit
 //on recupère la valeur de l'input
-console.log(inputfield.value);
+// console.log(inputfield.value);
+
+//on envoie la valeur dans ul- ` sans ecraser les items ulterieurs
+list.innerHTML +=`<li>${inputfield.value}</li>`;
+//on remet l'input à 0 
+inputfield.value = "";
 });
     
+list.addEventListener("click", (e) => {
+    if(e.target.classList.contains("checked")){
+        // /:supprimer au click
+        // e.target.remove();
+        e.target.remove();
+    } else {
+        // injecter classlist checked config css
+        e.target.classList.add("checked");
+
+    };
+
+});
